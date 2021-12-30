b4c1569fa22c7dc7025852a0e3e9992433d812ee
chenxianrui <2463019831@qq.com>
2021-12-30 15:01:16 +0800

    ..

---
+++ src/main/java/code/java/essay/EssayApplication.java
@@ -0,0 +1,13 @@
+package code.java.essay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EssayApplication.class, args);
+    }
+
+}
