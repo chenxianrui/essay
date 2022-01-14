fa69c8801a8b5567955ed273a7335dfac616dc3e
chenxianrui <2463019831@qq.com>
2022-01-14 22:31:10 +0800

    .

--- doc/实习中遇到的一些问题以及思考/线上打印日志为何不用System.out.println？.md
+++ doc/实习中遇到的一些问题以及思考/线上打印日志为何不用System.out.println？.md
@@ -4,10 +4,13 @@
 1. System.out.println()底层源码实现。
 2. Log4j作为日志工具比System.out.println()拥有哪些优点。
 
+#### System.out.println()的作用
+   
+
 #### System.out.println()源码
 
 
 #### Log4j的优点
 
-#### 总结
+#### 总结 
 
---
+++ src/main/java/code/java/essay/doc/SOUTClass.java
@@ -0,0 +1,12 @@
+package code.java.essay.doc;
+
+/**
+ * @Author cxr
+ * @Date 2022/1/6 22:01
+ */
+public class SOUTClass {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
